89d9f9ec777cc0359fefb81589d3adec5faf4aed
rao119349 <111695718+rao119349@users.noreply.github.com>
2022-09-16 22:21:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-bkskhv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-bkskhv)
---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  const [EmpName, SetEmpName] = React.useState('');
+  // console.log(EmpName);
+  function valChange(event) {
+    SetEmpName(event.target.value);
+  }
+
+  let txtLength = EmpName.length;
+  // document.getElementById('demo').innerHTML = txtLength;
+  let extratxt = '';
+  let greet = '';
+  console.log(txtLength);
+  console.log(extratxt);
+
+  if (txtLength > 20) {
+    greet = 'Text Length is exceeding 20 characters.';
+    extratxt = 20 - txtLength;
+  }
+
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <input
+        type="text"
+        name="employeename"
+        value={EmpName}
+        onChange={valChange}
+      />{' '}
+      &nbsp; <label className="error">{extratxt}</label>
+      <br />
+      {/* <label>{EmpName}</label> */}
+      <span className="error">{greet}</span>
+      {/* <p id="demo">{txtLength}</p> */}
+    </div>
+  );
+}
